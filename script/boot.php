1534794ab014ac5b42fddd8529f98cce73beb7e0
Justin Palmer <justin@mediaslave.net>
2011-02-14 18:15:19 -0800

    Initial commit.

---
+++ script/boot.php
@@ -0,0 +1,12 @@
+<?php
+//Register the autoload function for the command line scripts
+spl_autoload_register('cmd_autoload');
+//Boot up the framework.
+include __DIR__ . '/../config/boot.php';
+//Include the migrations folder.
+add_include_directory(__DIR__ . '/../db/migrations');
+//Register the autoload for migrations.
+function cmd_autoload($class_name){
+	$file = $class_name . '.migration.php';
+	$included = @include_once($file);
+}
